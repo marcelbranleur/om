681544b1bd32b10d0159585d9f4a556a31dee335
Alvine <alvine@tamkin.se>
2018-01-11 16:11:16 +0100

    Images and text on bloglisting

--- template-parts/content-excerpt.php
+++ template-parts/content-excerpt.php
@@ -11,19 +11,17 @@
 
 <div class="blog-post">
 
-	<?php	the_title( '<h4>', '</h4>' ); ?>	
+	<?php	the_title( '<h3>', '</h3>' ); ?>	
 
 	<?php if (has_post_thumbnail()) { ?>	
-		<?php the_post_thumbnail('large', array('class' => 'img-fluid w-100')); ?>
-		<?php echo '<a href="' . esc_url( get_permalink() ) . '" class="blog-link">Read more &rarr;</a>'; ?> 			
-	<?php }	else { ?>
+		<?php the_post_thumbnail('large', array('class' => 'attachment-thumbnail img-fluid')); ?>
+	<?php }	?>
 		<p>
 		<?php
 			echo excerpt(50);
 			echo '<a href="' . esc_url( get_permalink() ) . '" class="blog-link">Read more &rarr;</a>'; 
 		?>
 		</p>
-	<?php } ?>
 
 </div>
 
