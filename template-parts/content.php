130634778116cacdeb5b346aee9d10617a074930
Alvine <alvine@tamkin.se>
2018-01-17 09:38:04 +0100

    Removed featured image from post page

--- template-parts/content.php
+++ template-parts/content.php
@@ -12,10 +12,6 @@
 <div class="blog-post-full">
 	<?php the_title( '<h3>', '</h3>' ); ?>
 
-	<?php if (has_post_thumbnail()) { ?>	
-		<?php	the_post_thumbnail('large', array('class' => 'attachment-full')); ?>
-	<?php }	?>
-
 	<?php the_content(); ?>
 
 </div>
