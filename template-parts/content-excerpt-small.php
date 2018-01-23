1575dab2b58ed22e3175408335ac2786efe12a23
Alvine <alvine@tamkin.se>
2018-01-23 13:10:44 +0100

    Image size for img excerpt

--- template-parts/content-excerpt-small.php
+++ template-parts/content-excerpt-small.php
@@ -12,7 +12,7 @@
 <div class="blog-post-img">
 	<?php echo '<a href="' . esc_url( get_permalink() ) . '">'; ?>
 	<?php if (has_post_thumbnail()) { ?>	
-		<?php the_post_thumbnail('large', array('class' => 'img-fluid')); ?>
+		<?php the_post_thumbnail('slider', array('class' => 'img-fluid')); ?>
 	<?php } ?>
 	<?php	the_title( '<h3>', '</h3>' ); ?>	
 	<?php echo '</a>'; ?>
